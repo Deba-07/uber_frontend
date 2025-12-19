00f65a8669ebe5adada7222c6054160e1cc3a5d1
Deba-07 <dynamicdeba07@gmail.com>
2025-12-19 11:54:11 +0530

    Add CaptainLogin and CaptainRegister components for captain authentication

---
+++ src/pages/CaptainLogin.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const CaptainLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [captainData, setCaptainData] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setCaptainData({
+      email: email,
+      password: password,
+    });
+    console.log(captainData);
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="p-7 h-screen flex flex-col justify-between">
+      <div>
+        <img className="w-16 mb-6" src="/uber-driver.png" alt="logo" />
+        <form onClick={(e) => submitHandler(e)}>
+          <h3 className="text-lg font-medium mb-2">What's Your Email</h3>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+            placeholder="email@example.com"
+            className="bg-[#eeeeee] rounded border px-4 py-2 mb-7 w-full text-lg placeholder:text-sm"
+          />
+          <h3 className="text-lg font-medium mb-2">Enter Your Password</h3>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            placeholder="password"
+            className="bg-[#eeeeee] rounded border px-4 py-2 mb-7 w-full text-lg placeholder:text-sm"
+          />
+          <button className="bg-[#10b461] text-white font-semibold rounded border px-4 py-2 mb-7 w-full text-lg placeholder:text-sm">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          Get behind the wheel and earn{" "}
+          <Link to="/captain-signup" className="text-blue-500">
+            become a Captain today!
+          </Link>
+        </p>
+      </div>
+      <div>
+        <Link to='/login' className="bg-[#111] flex items-center justify-center text-white font-semibold rounded border px-4 py-2 mb-5 w-full text-lg placeholder:text-sm">
+          Sign in as User
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainLogin;
