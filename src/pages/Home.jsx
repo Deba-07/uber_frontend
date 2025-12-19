ae712ef7aa012d903d222b7f9f87908dc37d2400
Deba-07 <dynamicdeba07@gmail.com>
2025-12-19 11:53:51 +0530

    Refactor Home component to use React Router for navigation and update background styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div className="h-screen pt-8 w-full flex flex-col justify-between">
-        <img className="w-16 ml-8" src="/uber-logo.png" alt="logo" />
-        <div className="py-5 px-5 pb-7">
-          <h2 className="text-3xl font-bold">Get Started with Uber</h2>
-          <button className="w-full bg-black text-white py-3 rounded mt-4">
-            Continue
-          </button>
-        </div>
+    <div className="min-h-screen pt-8 w-full flex flex-col justify-between bg-top bg-cover bg-[url('/home.avif')]">
+      <img className="w-16 ml-8" src="/uber-logo.png" alt="logo" />
+      <div className="py-5 px-5 pb-7">
+        <h2 className="text-3xl font-bold">Get Started with Uber</h2>
+        <Link to="/login" className="flex items-center justify-center w-full bg-black text-white py-3 rounded mt-4">
+          Continue
+        </Link>
+      </div>
     </div>
   );
 };
