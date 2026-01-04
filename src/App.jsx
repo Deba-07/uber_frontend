e10456c1d24b2cba5b018db4096cf3583a0ebba3
Deba-07 <dynamicdeba07@gmail.com>
2026-01-04 18:39:34 +0530

    add user logout and captain logout route als implement protected wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,60 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import UserLogin from './pages/UserLogin'
-import UserRegister from './pages/UserRegister'
-import CaptainLogin from './pages/CaptainLogin'
-import CaptainRegister from './pages/CaptainRegister'
-import Start from './pages/Start'
-import CaptainHome from './pages/CaptainHome'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserRegister from "./pages/UserRegister";
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainRegister from "./pages/CaptainRegister";
+import Start from "./pages/Start";
+import CaptainHome from "./pages/CaptainHome";
+import UserProtectWrapper from "./pages/UserProtectWrapper";
+import UserLogout from "./pages/UserLogout";
+import CaptainProtectWrapper from "./pages/CaptainProtectWrapper";
+import CaptainLogout from "./pages/CaptainLogout";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<UserLogin />} />
-        <Route path='/signup' element={<UserRegister />} />
-        <Route path='/captain-login' element={<CaptainLogin />} />
-        <Route path='/captain-signup' element={<CaptainRegister />} />
-        <Route path='/home' element={<Start />} />
-        <Route path='/captain-home' element={<CaptainHome />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserRegister />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainRegister />} />
+        <Route
+          path="/home"
+          element={
+            <UserProtectWrapper>
+              <Start />
+            </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="/user/logout"
+          element={
+            <UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="/captain-home"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainHome />
+            </CaptainProtectWrapper>
+          }
+        />
+        <Route
+          path="/captain/logout"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainLogout />
+            </CaptainProtectWrapper>
+          }
+        />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
