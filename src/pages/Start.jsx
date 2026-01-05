b90941b29910cabf3781f756a4be9426dde92f1b
Deba-07 <dynamicdeba07@gmail.com>
2026-01-05 18:09:34 +0530

    update user home screen and implement  live track and location search panel

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,9 +1,129 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import LiveTracking from "../../components/LiveTracking";
+import LocationSearchPanel from "../../components/LocationSearchPanel";
 
 const Start = () => {
-  return (
-    <div>Start</div>
-  )
-}
+  const [panelOpen, setpanelOpen] = useState(false);
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [activeField, setActiveField] = useState(null);
+  const [pickupSuggestions, setPickupSuggestions] = useState([]);
+  const [destinationSuggestions, setDestinationSuggestions] = useState([]);
+  const [vehiclePanel, setVehiclePanel] = useState(false);
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
 
-export default Start
+  const handlePickupChange = (e) => {};
+
+  const handleDestinationChange = (e) => {};
+
+  async function findTrip() {}
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  useGSAP(
+    function () {
+      if (panelOpen) {
+        gsap.to(panelRef.current, {
+          height: "70%",
+          padding: 24,
+        });
+        gsap.to(panelCloseRef.current, {
+          opacity: 1,
+        });
+      } else {
+        gsap.to(panelRef.current, {
+          height: "0%",
+          padding: 0,
+        });
+        gsap.to(panelCloseRef.current, {
+          opacity: 0,
+        });
+      }
+    },
+    [panelOpen]
+  );
+  return (
+    <div className="h-screen relative overflow-hidden">
+      <img
+        className="w-16 absolute left-5 top-5"
+        src="/uber-transparent.png"
+        alt="logo"
+      />
+
+      <div className="h-screen w-screen">
+        <LiveTracking />
+      </div>
+      <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
+        <div className="h-[30%] p-6 bg-white relative">
+          <h5
+            ref={panelCloseRef}
+            onClick={() => {
+              setpanelOpen(false);
+            }}
+            className="absolute opacity-0 right-6 top-6 text-2xl"
+          >
+            <i className="ri-arrow-down-wide-line"></i>
+          </h5>
+          <h4 className="text-2xl font-semibold">Find a trip</h4>
+          <form
+            className="relative py-3"
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+          >
+            <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div>
+            <input
+              onClick={() => {
+                setpanelOpen(true);
+                setActiveField("pickup");
+              }}
+              value={pickup}
+              onChange={handlePickupChange}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full"
+              type="text"
+              placeholder="Add a pick-up location"
+            />
+            <input
+              onClick={() => {
+                setpanelOpen(true);
+                setActiveField("destination");
+              }}
+              value={destination}
+              onChange={handleDestinationChange}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3"
+              type="text"
+              placeholder="Enter your destination"
+            />
+          </form>
+          <button
+            onClick={findTrip}
+            className="bg-black text-white px-4 py-2 rounded-lg mt-3 w-full"
+          >
+            Find Trip
+          </button>
+        </div>
+        <div ref={panelRef} className="bg-white h-0">
+          <LocationSearchPanel
+            suggestions={
+              activeField === "pickup"
+                ? pickupSuggestions
+                : destinationSuggestions
+            }
+            setPanelOpen={setpanelOpen}
+            setVehiclePanel={setVehiclePanel}
+            setPickup={setPickup}
+            setDestination={setDestination}
+            activeField={activeField}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
